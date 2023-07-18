c10810c23346e32ea1b11416c671643d00eb1be1
Zhongfa Wang <Zhongfa_Wang@outlook.com>
2023-07-18 12:07:51 +0000

    Modify the CMakelists file to let the building correct insert debug flags "-g" Successfully dynamically count the branch numbers

--- InstCount/InstCount.cpp
+++ InstCount/InstCount.cpp
@@ -1,69 +1,63 @@
 #include "llvm/Analysis/InstCount.h"
 #include "llvm/ADT/Statistic.h"
 #include "llvm/Analysis/Passes.h"
+#include "llvm/IR/InstVisitor.h"
 #include "llvm/InitializePasses.h"
 #include "llvm/Support/Debug.h"
 #include "llvm/Support/ErrorHandling.h"
-#include "llvm/IR/InstVisitor.h"
 
-#include "llvm/Pass.h"
 #include "llvm/IR/Function.h"
-#include "llvm/Support/raw_ostream.h"
+#include "llvm/IR/IRBuilder.h"
 #include "llvm/IR/LegacyPassManager.h"
+#include "llvm/IR/Module.h"
+#include "llvm/Pass.h"
+#include "llvm/Support/raw_ostream.h"
 #include "llvm/Transforms/IPO/PassManagerBuilder.h"
+#include <iostream>
+#include <llvm-14/llvm/IR/IRBuilder.h>
+#include <llvm-14/llvm/IR/Instruction.h>
 #include <llvm/IR/BasicBlock.h>
 #include <map>
 #include <typeinfo>
-#include <iostream>
-#include "llvm/IR/Module.h"
 
 using namespace llvm;
 using namespace std;
 
-namespace
-{
-    struct InstCount : public ModulePass
-    {
-        static char ID;
-        InstCount() : ModulePass(ID) {}
-        bool runOnModule(Module &M) override
-        {
-            map<string, int> opcode_map;
-            // std::cout << typeid(M.getName()).name() << std::endl;
-            errs() << "Module: " << M.getName() << "\n";
-            for (Module::iterator FBegin = M.begin(), FEnd = M.end(); FBegin != FEnd; ++FBegin)
-            {
-                for (Function::iterator BB = FBegin->begin(), BEnd = FBegin->end(); BB != BEnd; ++BB)
-                {
-                    for (BasicBlock::iterator instBegin = BB->begin(), instEnd = BB->end(); instBegin != instEnd; ++instBegin)
-                    {
-                        if (opcode_map.find(instBegin->getOpcodeName()) == opcode_map.end())
-                        {
-                            opcode_map[instBegin->getOpcodeName()] = 1;
-                        }
-                        else
-                        {
-                            opcode_map[instBegin->getOpcodeName()] += 1;
-                        }
-                    }
-                }
-            }
+namespace {
+struct InstCount : public ModulePass {
+  static char ID;
+  InstCount() : ModulePass(ID) {}
+  bool runOnModule(Module &M) override {
 
-            for (map<string, int>::iterator i = opcode_map.begin(), e = opcode_map.end(); i != e; ++i)
-            {
-                // if (i->first == "br")
-                // {
-                //     errs() << i->first << ":::" << i->second << "\n";
-                // }
-                errs() << i->first << ": " << i->second << "\n";
-            }
+    // declare and initialize the global variable BranchNumber
+    auto type = IntegerType::getInt32Ty(M.getContext());
+    auto constantInt = ConstantInt::getIntegerValue(type, APInt(32, 0));
+    auto BranchNumber = new GlobalVariable(
+        M, type, false, GlobalValue::CommonLinkage, constantInt, "br_number");
 
-            opcode_map.clear();
+    for (Module::iterator FBegin = M.begin(), FEnd = M.end(); FBegin != FEnd;
+         ++FBegin) {
+      for (Function::iterator BB = FBegin->begin(), BEnd = FBegin->end();
+           BB != BEnd; ++BB) {
+        for (BasicBlock::iterator instBegin = BB->begin(), instEnd = BB->end();
+             instBegin != instEnd; ++instBegin) {
 
-            return false;
+          // Insert at the point where the instruction `op` appears.
+          IRBuilder<> IR(dyn_cast<Instruction>(instBegin));
+          // If the instruction is a branch, the BranchNumber++
+          if (std::string(instBegin->getOpcodeName()) == "br") {
+            LoadInst *Load = IR.CreateLoad(type, BranchNumber, "br_number");
+            Value *Inc = IR.CreateAdd(IR.getInt32(1), Load);
+            StoreInst *Store = IR.CreateStore(Inc, BranchNumber);
+          }
         }
+      }
+    }
 
-    };//End of struct InstCount
+    return false;
+  }
+
+}; // End of struct InstCount
 
 } // end of anonymous namespace
 
@@ -73,7 +67,8 @@ static RegisterPass<InstCount> X("InstCount", "Test Hello World Pass",
                                  false /*Only loooks at CFG*/,
                                  false /*Analysis Pass*/);
 
-static RegisterStandardPasses Y(
-    PassManagerBuilder::EP_EarlyAsPossible,
-    [](const PassManagerBuilder &Builder, legacy::PassManagerBase &PM)
-    { PM.add(new InstCount()); });
+static RegisterStandardPasses Y(PassManagerBuilder::EP_EarlyAsPossible,
+                                [](const PassManagerBuilder &Builder,
+                                   legacy::PassManagerBase &PM) {
+                                  PM.add(new InstCount());
+                                });
